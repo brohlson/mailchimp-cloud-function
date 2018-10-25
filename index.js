b993cb1c8a370c4e0db8c728dd72e58db54ee2e4
Chase Ohlson <chase@chaseohlson.com>
2018-10-24 17:55:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+
+var Mailchimp = require("mailchimp-api-v3");
+var mailchimp = new Mailchimp(API_KEY);
+
+exports.mailchimpSubscribe = (req, res) => {
+  let email = req.query.email || req.body.email;
+
+  mailchimp
+    .post("/lists/346294c571/members", {
+      email_address: email,
+      status: "subscribed"
+    })
+    .then(function(results) {
+      res.status(200).send(results);
+    })
+    .catch(function(err) {
+      res.status(err.status).send(err);
+    });
+};
